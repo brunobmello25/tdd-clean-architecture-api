e76df75aa3f8fa6f11f1a7df31eeec0b5024f883
Bruno Mello <bruno.barros.mello@gmail.com>
2020-09-27 18:56:07 -0300

    refactor: rename CeateAccount to Createuser

--- src/presentation/controllers/SignUp/SignUpProtocols.ts
+++ src/presentation/controllers/SignUp/SignUpProtocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols';
 export * from '../../protocols/emailValidator';
-export * from '../../../domain/useCases/createAccount';
+export * from '../../../domain/useCases/createUser';
 export * from '../../../domain/models/User';
