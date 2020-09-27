d6d7f9d3065d27f504e2bbcdf5bd9cf980f02772
Bruno Mello <bruno.barros.mello@gmail.com>
2020-09-27 18:57:26 -0300

    test: ensure SignUpController returns 500 if CreateUser throws

--- src/presentation/controllers/SignUp/signUpController.spec.ts
+++ src/presentation/controllers/SignUp/signUpController.spec.ts
@@ -218,4 +218,25 @@ describe('SignUpController', () => {
       password: httpRequest.body.password,
     });
   });
+
+  test('should return 500 if CreateUser throws', () => {
+    const { sut, createUserStub } = createSut();
+    jest.spyOn(createUserStub, 'create').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
 });
